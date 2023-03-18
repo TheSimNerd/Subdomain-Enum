586f812a7e13193f1bf018b09f84c22528ab1a35
David Griffus <david@cryptolic.org>
2023-03-18 10:48:32 -0400

    Updated line 8 sys.argv

--- subdomain.py
+++ subdomain.py
@@ -5,7 +5,7 @@ sub_list = open("wordlist2.txt").read()
 subdoms = sub_list.splitlines()
 
 for sub in subdoms:
-    sub_domains = f"http://{sub}.{sys.argv[2]}"
+    sub_domains = f"http://{sub}.{sys.argv[1]}"
 
     try:
         requests.get(sub_domains)
